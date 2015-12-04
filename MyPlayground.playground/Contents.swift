7f2ebbbde91a5ce58d1f809f9bb9cce5ce17073b
Doracool <751379858@qq.com>
2015-12-04 09:38:21 +0800

    Swift初学,值转字符串，创建和使用字典数组

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,33 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+let myValue:Float = 4
+
+let label = "The width is"
+let width = 94
+
+let widthLabel = label + String(width)
+
+let apples = 2
+let aranges = 4
+let appleSummary = "I have \(apples) apples"
+let fruitSummary = "I have \(apples + aranges) pieces of fruit"
+
+let name = "Lihua"
+let number:Float = 3
+
+let hello = "My name is \(name) I have \(number) apple"
+
+var shoopingList = ["cat","water","tulips","blue"]
+shoopingList[1] = "bottle of water"
+
+var occcupations = [
+"Malcolm":"Captain",
+"Kaylee":"Mechanic",
+]
+occcupations["Jayne"] = "Public Relations"
+
+occcupations["Kaylee"]
